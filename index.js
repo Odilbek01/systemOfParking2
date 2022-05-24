7c1cec990e18010438bf5a7d99eaa59b459ac5d2
Odilbek <odilbek8555@gmail.com>
2022-05-24 18:01:44 +0500

    Version 1.2

--- index.js
+++ index.js
@@ -2,24 +2,40 @@ let enter = document.querySelector(`.innerEnter`)
 let exit = document.querySelector(`.innerExit`)
 let prev = document.querySelector(`.prev`)
 let next = document.querySelector(`.next`)
-
-
-prev.addEventListener(`click` , ()=>{
+let price = document.querySelector('.price');
+prev.addEventListener(`click`, () => {
     const dateEnter = new Date();
-    const min = dateEnter.getMinutes()
     const hour = dateEnter.getHours()
+    const min = dateEnter.getMinutes()
     const second = dateEnter.getSeconds()
     // console.log(`${hour}:${min}`);
     enter.innerHTML = `${hour}:${min}:${second}`
-    next.addEventListener(`click` , ()=>{
+    next.addEventListener(`click`, () => {
         const dateExit = new Date();
-        const minexit = dateExit.getMinutes()
         const hourexit = dateExit.getHours()
+
+        const minexit = dateExit.getMinutes()
+
         const secondexit = dateExit.getSeconds()
         exit.innerHTML = `${hourexit}:${minexit}:${secondexit}`
-        console.log(hourexit-hour);
-        console.log(minexit-min);
-        console.log(secondexit-second);
+        const absMin = Math.abs(minexit - min)
+        const absHours = hourexit - hour
+        if (absHours >= 1) {
+            for (let i = 0; i <= 24; i++) {
+                if (absHours <= i) {
+                    price.innerHTML = `${i} soat ${absMin} daqiqa turdingiz <p>Siznig tolovingiz:${i * 4 + 5}$ </p> `
+                    break
+                }
+            }
+            return
+        }
+        if (absMin > 0 || absMin < 59) {
+            if (absMin < 1) {
+                price.innerHTML = `${absMin} daqiqa turdingiz <p>Siznig tolovingiz: 2$</p> `
+            } else {
+                price.innerHTML = `${absMin} daqiqa turdingiz <p>Siznig tolovingiz: 5$</p> `
+            }
+        }
 
 
 
